072a19fbfa3ffe252639944c7e6921528c68ca49
Roger <jaeggir@gmail.com>
2012-10-24 13:00:12 +0200

    introduction of sherlockActionBar

--- src/ch/rogerjaeggi/txt/PageActivity.java
+++ src/ch/rogerjaeggi/txt/PageActivity.java
@@ -1,61 +1,49 @@
 package ch.rogerjaeggi.txt;
 
-import android.annotation.TargetApi;
-import android.app.ActionBar;
-import android.app.ActionBar.OnNavigationListener;
-import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.SpinnerAdapter;
+import ch.rogerjaeggi.txt.R.anim;
 
-public class PageActivity extends Activity implements OnNavigationListener {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+public class PageActivity extends SherlockActivity {
 	
 	private static final String BASE_URL = "http://www.teletext.ch/dynpics/";
 	
-	private static final String TAG = "txt.pageActivity";
-	
 	AsyncTask<Void, Void, Bitmap> loadPageTask;
 	
-    @TargetApi(11)
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
 		setTitle("");
-        
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(false);
-        actionBar.setDisplayUseLogoEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-        SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(actionBar.getThemedContext(), R.array.channels, android.R.layout.simple_spinner_dropdown_item);
-        actionBar.setListNavigationCallbacks(spinnerAdapter, this);
-        
-        runLoadPageTask(BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);        
+        runLoadPageTask(null, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);        
     }
-
-	@Override
-	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-		Log.d(TAG, "itemPosition=" + itemPosition + ", itemId=" + itemId);
-		EChannel channel = EChannel.values()[itemPosition];
-		getTxtApplication().setCurrentChannel(channel);
-		getTxtApplication().setCurrentPage(100);
-		runLoadPageTask(BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);
-		return true;
-	}
     
-    private void runLoadPageTask(String baseUrl, final int page, int subIndex) {
+    private void runLoadPageTask(final MenuItem item, String baseUrl, final int page, int subIndex) {
     	if (loadPageTask != null) {
     		loadPageTask.cancel(true);
     		loadPageTask = null;
     	}
 		loadPageTask = new LoadPageTask(baseUrl, page, subIndex) {
+			
+			@Override
+			protected void onCancelled() {
+				loadPageTask = null;
+				cancelAnimation();
+				invalidateOptionsMenu();
+				// TODO
+				super.onCancelled();
+			}
 			
 			@Override
 		    protected void onPostExecute(Bitmap result) {
@@ -67,15 +55,25 @@ public class PageActivity extends Activity implements OnNavigationListener {
 				findViewById(R.id.loadingText).setVisibility(View.GONE);
 				TxtApplication app = (TxtApplication) getApplication();
 				app.setCurrentPage(page); // TODO
+				
+				cancelAnimation();
 				invalidateOptionsMenu();
 		    }
+			
+			private void cancelAnimation() {
+				if (item != null && item.getActionView() != null) {
+					item.getActionView().clearAnimation();
+					item.setActionView(null);
+				}
+			}
 		};
 		loadPageTask.execute();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_page, menu);
+    	super.onCreateOptionsMenu(menu);
+    	getSupportMenuInflater().inflate(R.menu.activity_page, menu);
         return true;
     }
     
@@ -83,17 +81,26 @@ public class PageActivity extends Activity implements OnNavigationListener {
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
     		case R.id.menu_refresh:
-                item.setActionView(R.layout.indeterminate_progress_action);
     	    	item.setEnabled(false);
-    			runLoadPageTask(BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);    
+    	    	
+    	    	LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	        ImageView iv = (ImageView) inflater.inflate(R.layout.refresh_action_view, null);
+
+    	        Animation rotation = AnimationUtils.loadAnimation(this, anim.refresh_animation);
+    	        rotation.setRepeatCount(Animation.INFINITE);
+    	        iv.startAnimation(rotation);
+
+    	        item.setActionView(iv);
+    	    	
+    			runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);    
     			return true;
     		case R.id.menu_backwards:
     	    	item.setEnabled(false);
-    			runLoadPageTask(BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() - 1, 0);  
+    			runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() - 1, 0);  
     			return true;
     		case R.id.menu_forewards:
     	    	item.setEnabled(false);
-    			runLoadPageTask(BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() + 1, 0);  
+    			runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() + 1, 0);  
     			return true;
     		default:
     			return super.onOptionsItemSelected(item);
