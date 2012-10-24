4eb89f53a071cbc22acc3cb9fb184ddcf34dc1ed
Roger <jaeggir@gmail.com>
2012-10-24 15:35:10 +0200

    swipe left and right

--- src/ch/rogerjaeggi/txt/PageActivity.java
+++ src/ch/rogerjaeggi/txt/PageActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
@@ -18,31 +21,77 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
-public class PageActivity extends SherlockActivity {
-	
+public class PageActivity extends SherlockActivity implements OnClickListener {
+
 	private static final int GO_TO_CODE = 77;
-	
+
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
 	private static final String BASE_URL = "http://www.teletext.ch/dynpics/";
-	
+
 	AsyncTask<Void, Void, Bitmap> loadPageTask;
-	
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_page);
 		setTitle(getTxtApplication().getCurrentChannel().getId() + " - " + getTxtApplication().getCurrentPage());
-		View page = findViewById(R.id.page);
-		page.setOnClickListener(new OnClickListener() {
-			
+		
+		// Gesture detection
+		final GestureDetector gestureDetector = new GestureDetector(this, new SimpleOnGestureListener() {
+
 			@Override
-			public void onClick(View v) {
+			public boolean onSingleTapConfirmed(MotionEvent e) {
 				Intent intent = new Intent(PageActivity.this, GoToActivity.class);
 				startActivityForResult(intent, GO_TO_CODE);
+				return true;
 			}
+
+			@Override
+			public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+				try {
+					if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
+						return false;
+					} else if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+						if (getTxtApplication().getCurrentPage() < 899) {
+							runLoadPageTask(null, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() + 1, 0);
+							return true;
+						}
+					} else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+						if (getTxtApplication().getCurrentPage() > 100) {
+							runLoadPageTask(null, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() - 1, 0);
+							return true;
+						}
+					}
+				} catch (Exception e) {
+					// nothing
+				}
+				return false;
+			}
+
 		});
-        runLoadPageTask(null, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);        
-    }
-	
+
+		View.OnTouchListener gestureListener = new View.OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
+			}
+		};
+
+		View page = findViewById(R.id.page);
+		page.setOnClickListener(this);
+		page.setOnTouchListener(gestureListener);
+
+		runLoadPageTask(null, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);
+	}
+
+	@Override
+	public void onClick(View v) {
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == GO_TO_CODE && RESULT_OK == resultCode) {
@@ -51,14 +100,14 @@ public class PageActivity extends SherlockActivity {
 			super.onActivityResult(requestCode, resultCode, data);
 		}
 	}
-    
-    private void runLoadPageTask(final MenuItem item, String baseUrl, final int page, int subIndex) {
-    	if (loadPageTask != null) {
-    		loadPageTask.cancel(true);
-    		loadPageTask = null;
-    	}
+
+	private void runLoadPageTask(final MenuItem item, String baseUrl, final int page, int subIndex) {
+		if (loadPageTask != null) {
+			loadPageTask.cancel(true);
+			loadPageTask = null;
+		}
 		loadPageTask = new LoadPageTask(baseUrl, page, subIndex) {
-			
+
 			@Override
 			protected void onCancelled() {
 				loadPageTask = null;
@@ -67,9 +116,9 @@ public class PageActivity extends SherlockActivity {
 				// TODO
 				super.onCancelled();
 			}
-			
+
 			@Override
-		    protected void onPostExecute(Bitmap result) {
+			protected void onPostExecute(Bitmap result) {
 				loadPageTask = null;
 				if (result == null) {
 					Toast.makeText(PageActivity.this, "Page not found", Toast.LENGTH_LONG).show();
@@ -85,8 +134,8 @@ public class PageActivity extends SherlockActivity {
 				}
 				cancelAnimation();
 				invalidateOptionsMenu();
-		    }
-			
+			}
+
 			private void cancelAnimation() {
 				if (item != null && item.getActionView() != null) {
 					item.getActionView().clearAnimation();
@@ -95,76 +144,76 @@ public class PageActivity extends SherlockActivity {
 			}
 		};
 		loadPageTask.execute();
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	super.onCreateOptionsMenu(menu);
-    	getSupportMenuInflater().inflate(R.menu.activity_page, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-    		case R.id.menu_refresh:
-    	    	item.setEnabled(false);
-    	    	
-    	    	LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    	        ImageView iv = (ImageView) inflater.inflate(R.layout.refresh_action_view, null);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getSupportMenuInflater().inflate(R.menu.activity_page, menu);
+		return true;
+	}
 
-    	        Animation rotation = AnimationUtils.loadAnimation(this, anim.refresh_animation);
-    	        rotation.setRepeatCount(Animation.INFINITE);
-    	        iv.startAnimation(rotation);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_refresh:
+				item.setEnabled(false);
 
-    	        item.setActionView(iv);
-    	    	
-    			runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);    
-    			return true;
-    		case R.id.menu_backwards:
-    	    	item.setEnabled(false);
-    			runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() - 1, 0);  
-    			return true;
-    		case R.id.menu_forewards:
-    	    	item.setEnabled(false);
-    			runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() + 1, 0);  
-    			return true;
-    		case R.id.menu_goto:
-    			Intent intent = new Intent(PageActivity.this, GoToActivity.class);
+				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				ImageView iv = (ImageView) inflater.inflate(R.layout.refresh_action_view, null);
+
+				Animation rotation = AnimationUtils.loadAnimation(this, anim.refresh_animation);
+				rotation.setRepeatCount(Animation.INFINITE);
+				iv.startAnimation(rotation);
+
+				item.setActionView(iv);
+
+				runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage(), 0);
+				return true;
+			case R.id.menu_backwards:
+				item.setEnabled(false);
+				runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() - 1, 0);
+				return true;
+			case R.id.menu_forewards:
+				item.setEnabled(false);
+				runLoadPageTask(item, BASE_URL + getTxtApplication().getCurrentChannel().getId() + "/", getCurrentPage() + 1, 0);
+				return true;
+			case R.id.menu_goto:
+				Intent intent = new Intent(PageActivity.this, GoToActivity.class);
 				startActivityForResult(intent, GO_TO_CODE);
 				return true;
-    		default:
-    			return super.onOptionsItemSelected(item);
-    	}
-    }
-    
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-    	MenuItem item = menu.findItem(R.id.menu_backwards);
-    	item.setEnabled(getCurrentPage() != 100);
-    	
-    	item = menu.findItem(R.id.menu_forewards);
-    	item.setEnabled(getCurrentPage() != 89); // TODO set background image
-    	
-    	item = menu.findItem(R.id.menu_refresh);
-    	item.setEnabled(true);
-    	
-    	return super.onPrepareOptionsMenu(menu);
-    }
-    
-    private TxtApplication getTxtApplication() {
-    	return (TxtApplication) getApplication();
-    }
-    
-    private int getCurrentPage() {
-    	return getTxtApplication().getCurrentPage();
-    }
-    
-    @Override
-    protected void onDestroy() {
-        if (loadPageTask != null) {
-        	loadPageTask.cancel(true);
-        }
-        super.onDestroy();
-    }
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem item = menu.findItem(R.id.menu_backwards);
+		item.setEnabled(getCurrentPage() != 100);
+
+		item = menu.findItem(R.id.menu_forewards);
+		item.setEnabled(getCurrentPage() != 89); // TODO set background image
+
+		item = menu.findItem(R.id.menu_refresh);
+		item.setEnabled(true);
+
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	private TxtApplication getTxtApplication() {
+		return (TxtApplication) getApplication();
+	}
+
+	private int getCurrentPage() {
+		return getTxtApplication().getCurrentPage();
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (loadPageTask != null) {
+			loadPageTask.cancel(true);
+		}
+		super.onDestroy();
+	}
 }
