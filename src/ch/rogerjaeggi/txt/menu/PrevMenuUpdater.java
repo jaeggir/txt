929b3e376c87b609655891cba3662565a9ff8975
Roger <jaeggir@gmail.com>
2013-02-21 22:26:23 +0100

    cleanup

--- src/ch/rogerjaeggi/txt/menu/PrevMenuUpdater.java
+++ src/ch/rogerjaeggi/txt/menu/PrevMenuUpdater.java
@@ -15,9 +15,12 @@ public class PrevMenuUpdater implements MenuUpdater {
 	
 	@Override
 	public void update(int page) {
-		boolean enabled = page > 100;
-		item.setEnabled(enabled);
-		item.setIcon(enabled ? R.drawable.ic_find_previous_holo_dark: R.drawable.ic_find_previous_holo_dark_disabled);
+		item.setEnabled(isEnabled(page));
+		item.setIcon(isEnabled(page) ? R.drawable.ic_find_previous_holo_dark: R.drawable.ic_find_previous_holo_dark_disabled);
+	}
+	
+	private boolean isEnabled(int page) {
+		return page > 100;
 	}
 	
 	@Override
